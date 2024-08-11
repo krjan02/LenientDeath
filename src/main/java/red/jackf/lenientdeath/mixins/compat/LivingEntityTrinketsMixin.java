f856a27f3b45fefbf2147a2b7cb662f5a6779cea
Jack Pearce <jack@jackf.red>
2024-08-11 23:18:15 +0100

    [fix] compat with trinkets 3.10

--- src/main/java/red/jackf/lenientdeath/mixins/compat/LivingEntityTrinketsMixin.java
+++ src/main/java/red/jackf/lenientdeath/mixins/compat/LivingEntityTrinketsMixin.java
@@ -1,27 +1,32 @@
 package red.jackf.lenientdeath.mixins.compat;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import red.jackf.lenientdeath.api.LenientDeathAPI;
 
+// TODO solve cleaner, make it handle @Unique or ask for a callback or smth
 @Mixin(value = LivingEntity.class, priority = 1200)
 @Pseudo
 public class LivingEntityTrinketsMixin {
-
-    @SuppressWarnings({"UnresolvedMixinReference", "MixinAnnotationTarget", "UnnecessaryQualifiedMemberReference"})
-    @Inject(method = "Lnet/minecraft/world/entity/LivingEntity;dropFromEntity(Lnet/minecraft/world/item/ItemStack;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;setDeltaMovement(DDD)V"),
+    @SuppressWarnings({"UnresolvedMixinReference", "MixinAnnotationTarget"})
+    @WrapOperation(
+            method = "dropFromEntity",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;"),
             require = 0)
-    private void lenientdeath$handleTrinketsItemEntities(ItemStack stack, CallbackInfo ci, @Local ItemEntity itemEntity) {
-        if (((Object) this) instanceof ServerPlayer serverPlayer)
-            LenientDeathAPI.INSTANCE.markDeathItem(serverPlayer, itemEntity, null);
+    private ItemEntity lenientdeath$handleTrinketsItemEntities(Player player, ItemStack stack, boolean dropAround, boolean throwerName, Operation<ItemEntity> original) {
+        ItemEntity entity = original.call(player, stack, dropAround, throwerName);
+
+        if (player instanceof ServerPlayer serverPlayer && entity != null)
+            LenientDeathAPI.INSTANCE.markDeathItem(serverPlayer, entity, null);
+
+        return entity;
     }
 }
